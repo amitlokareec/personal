197e39760130215286c1c205a398f1b77d1ab514
Amit Lokare <amitlokareec@gmail.com>
2025-07-12 09:58:57 +0100

    new Sorting ways and improved code

--- Learning/src/com/amit/learning/sliding_window/Anagram.java
+++ Learning/src/com/amit/learning/sliding_window/Anagram.java
@@ -8,12 +8,12 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Anagram {
-    public static boolean isAnagram(String s, String t) {
-        if (Objects.isNull(s) || Objects.isNull(t) || s.length() != t.length()) return false;
-        s = s.toLowerCase();
-        t = t.toLowerCase();
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
+    public static boolean isAnagram(String source, String tgt) {
+        if (Objects.isNull(source) || Objects.isNull(tgt) || source.length() != tgt.length()) return false;
+        source = source.toLowerCase();
+        tgt = tgt.toLowerCase();
+        char[] sChars = source.toCharArray();
+        char[] tChars = tgt.toCharArray();
         Arrays.sort(sChars);
         Arrays.sort(tChars);
         return Arrays.equals(sChars, tChars);
@@ -27,37 +27,24 @@ public class Anagram {
 
     }
 
-    public static List<Integer> findAllAnagramsInAString(String str, String findStr) {
-        if(Objects.isNull(str) || Objects.isNull(findStr) || str.length() < findStr.length()){
+    public static List<Integer> findAllAnagramsInAString(String targetString, String findStr) {
+        if(Objects.isNull(targetString) || Objects.isNull(findStr) || targetString.length() < findStr.length()){
             return null;
         }
         int[] findStrChars = new int[26];
         int[] strChars = new int[26];
         List<Integer> indexes = new ArrayList<>();
-        for(char c: findStr.toCharArray()){
-            findStrChars[c-'a']++;
+        for(char character: findStr.toCharArray()){//findStr = "aab"
+            findStrChars[character -'a']++;//it will be like for a it will 0, for b 1 and so on, [2,1,0,0,0,0,0,0.....]
         }
-        for (int right = 0; right < str.length(); right++) {
-            int count = 0;
-            strChars[str.charAt(right)-'a']++;
-            if (right >= findStr.length()) {
-                strChars[str.charAt(right - findStr.length()) - 'a']--;
+        for (int rightIndex = 0; rightIndex < targetString.length(); rightIndex++) {
+            strChars[targetString.charAt(rightIndex)-'a']++;
+            if (rightIndex >= findStr.length()) {
+                strChars[targetString.charAt(rightIndex - findStr.length()) - 'a']--;
             }
             if (Arrays.equals(findStrChars, strChars)) {
-                indexes.add(right - findStr.length() + 1);
+                indexes.add(rightIndex - findStr.length() + 1);
             }
-            /* Arrays.equals(findStrChars, strChars) OR
-            for (int i = 0; i < 26; i++) {
-                if(findStrChars[i] != strChars[i]){
-                    count = 0;
-                    break;
-                }
-                count++;
-            }
-            if (count == findStrChars.length) {
-                indexes.add(right - findStr.length() + 1);
-            }*/
-
         }
         return indexes;
     }
